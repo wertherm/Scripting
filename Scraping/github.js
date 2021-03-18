e51aa5cf2c1120a2d7f5fdfafe2a5759b150c475
Werther <wertherm@gmail.com>
2021-03-18 12:25:11 -0300

    Ajustes script github

--- Scraping/github.js
+++ Scraping/github.js
@@ -1,4 +1,5 @@
-//Raspa githubers com mais de 100 repositórios do seu feed
+//Raspa githubers com mais de 100 repositórios do seu feed e já cria uma estrutura html.
+//Não é necessário expandir cada feed de quem você segue.
 
 var elements = document.querySelectorAll(".mr-3 > .Link--muted"); //Seleciona o "span" com a classe "mr-3" e seu elemento interno "a" com a classe "Link--muted"
     for (i in elements) {
@@ -7,5 +8,5 @@ var elements = document.querySelectorAll(".mr-3 > .Link--muted"); //Seleciona o 
         const regex = /1\d[1-9]|[2-9]\d{2,}|\d{4,}/g
 
         if (elements[i].innerText.match(regex) >= 100)
-            console.log(elements[i].href)
+            console.log('<a href="' + elements[i].href + '">Githuber ' + i + '</a><br />')
     }
