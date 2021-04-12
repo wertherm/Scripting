11bb65efbc672357558a7dcbc37ed614013776df
Verthones <wertherm@gmail.com>
2021-04-12 14:54:21 -0300

    Update CompSeg.ps1

--- WindowsOrganization/CompSeg.ps1
+++ WindowsOrganization/CompSeg.ps1
@@ -1,4 +1,8 @@
-# Compara a diferenca entre dois arquivos TXTs, util para ver quem voce segue que nao te segue no Instagram, LinkedIn e Twitter. Antes tem que fazer o scraper JS na rede-social alvo, para gerar o TXT.
+# Compare the difference between two TXT files, useful to see who follows you who doesn't follow you on Instagram, LinkedIn. Before you have to do the JS scraper on the target social network, to generate the TXTs
+
+#Scrapers
+#https://github.com/wertherm/Scripting/blob/master/Scraping/instagram.js
+#https://github.com/wertherm/Scripting/blob/master/Scraping/linkedin.js
 
 $Seguindo = Get-Content -Path Seguindo.txt
 $Seguidores = Get-Content -Path Seguidores.txt
